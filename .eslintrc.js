ce804da802217095e7a077138c7813bd1148e901
David <12832280+David-Else@users.noreply.github.com>
2020-09-04 11:13:32 +0100

    Remove duplicate rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,9 +15,7 @@ module.exports = {
       parser: "@typescript-eslint/parser",
       extends: ["plugin:@typescript-eslint/recommended"],
       rules: {
-        "no-unused-vars": "off",
         "@typescript-eslint/no-unused-vars": "off",
-        "no-empty-function": "off",
         "@typescript-eslint/no-empty-function": "off",
         "@typescript-eslint/ban-types": "off",
         "@typescript-eslint/no-explicit-any": "off",
